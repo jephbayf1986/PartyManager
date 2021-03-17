cbc097ba1fe51d63c84a3e6154c699b36b8f6e95
Jeph Bayfield <jeph_b@hotmail.com>
2021-03-17 08:22:47 +0000

    Refactored pipeline and add registration

---
+++ Core/PartyManager.Application/Shared/CQRS/CQRSStartup.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using PartyManager.Application.Shared.Validation;
+using System;
+using System.Linq;
+using System.Reflection;
+
+namespace PartyManager.Application.Shared.CQRS
+{
+    public static class CQRSStartup
+    {
+        public static void BindCQRSPipeline(this IServiceCollection services)
+        {
+            services.BindPipelineComponent(typeof(IQueryHandler<,>));
+            services.BindPipelineComponent(typeof(ICommandHandler<,>));
+            services.BindPipelineComponent(typeof(IValidator<>));
+        }
+
+        private static void BindPipelineComponent(this IServiceCollection services, Type componentInterface)
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+
+            var components = assembly.GetTypes()
+                                        .Where(x => x.GetInterfaces().Any(i => i.IsGenericType
+                                                                            && i.GetGenericTypeDefinition() == componentInterface));
+
+            foreach (var component in components)
+            {
+                services.AddScoped(component.GetInterfaces()
+                                          .First(i => i.IsGenericType && i.GetGenericTypeDefinition() == componentInterface), component);
+            }
+        }
+    }
+}
---
+++ Persistence/PartyManager.DAL/DALStartup.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using PartyManager.Application.Shared.DataAccess.Interfaces;
+using PartyManager.DAL.DataProviders;
+using PartyManager.DAL.Infrastructure;
+
+namespace PartyManager.DAL
+{
+    public static class DALStartup
+    {
+        public static void RegisterDataProviders(this IServiceCollection services)
+        {
+            services.AddTransient<IDbCore, DbCore>();
+
+            services.AddTransient<IPartyDataProvider, PartyDataProvider>();
+        }
+    }
+}
